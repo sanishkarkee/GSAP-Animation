1636aee17c1a6e75db7ec199f2e0b080ce1b1be0
sanish <notsansh@outlook.com>
2024-09-06 12:04:40 +0545

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-//NOTE: yesma change hune bhaneko center ko (x,y) value matra ho,starting ra ending point change hudaina
+/*NOTE:
+ 1) yesma change hune bhaneko center ko (x,y) value matra ho
+    starting ra ending point change hudaina
+ 2) overall bujda yesma 2ta actions hunxa i.e.
+   # "string" div bhitra mouse cursor lagda
+   # "string" div bahira mouse cursor lagda
+ 3) LOGIC: string bhitra mouse lagda "path" ko value change garne
+           string bahira cursor lagda  finalPath ko value set garne
+    */
 
 // var path = `M 10 100 Q 500 100 990 100`;
 
@@ -7,7 +15,9 @@
 // var string = document.querySelector('#string');
 
 // string.addEventListener('mousemove', (dets) => {
+// //'dets' bata dherai data aauxa tara yaha use hune bhaneko 'X','Y' axis ko value ho
 //   // (dets.x) => x-axis value    &&    (dets.y) => y-axis value
+
 //   path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`;
 
 //   gsap.to('svg path', {
@@ -26,17 +36,52 @@
 // });
 
 // -------------------------PRATICE-------------------------
-//Note: yesma change hune bhaneko center ko (x,y) value matra ho
-//starting ra ending point change hudaina
+/*NOTE:
+ 1) yesma change hune bhaneko center ko (x,y) value matra ho
+    starting ra ending point change hudaina
+ 2) overall bujda yesma 2ta actions hunxa i.e.
+   # "string" div bhitra mouse cursor lagda
+   # "string" div bahira mouse cursor lagda
+ 3) LOGIC: string bhitra mouse lagda "path" ko value change garne
+           string bahira cursor lagda  finalPath ko value set garne
+    */
+
+// let path = `M 10 100 Q 500 100 990 100`;
+
+// let finalPath = `M 10 100 Q 500 100 990 100`;
+
+// const string = document.querySelector('#string');
+
+// string.addEventListener('mousemove', (receivedValue) => {
+//   path = `M 10 100 Q ${receivedValue.x} ${receivedValue.y} 990 100`;
+
+//   gsap.to('svg path', {
+//     attr: { d: path },
+//     duration: 0.2,
+//     ease: 'power3.out',
+//   });
+// });
+
+// string.addEventListener('mouseleave', (receivedValue) => {
+//   gsap.to('svg path', {
+//     attr: { d: finalPath },
+//     duration: 4,
+//     ease: 'elastic.out(1.5,0.1)',
+//   });
+// });
+
+// ---------------------PRATICE 2--------------------------
 
 let path = `M 10 100 Q 500 100 990 100`;
 
 let finalPath = `M 10 100 Q 500 100 990 100`;
 
-const string = document.querySelector('#string');
+let string = document.querySelector('#string');
 
-string.addEventListener('mousemove', (receivedValue) => {
-  path = `M 10 100 Q ${receivedValue.x} ${receivedValue.y} 990 100`;
+string.addEventListener('mousemove', (mousemoveBataAakoData) => {
+  //'mousemoveBataAakoData' bata dherai data aauxa tara ya use hune bhaneko 'X','Y' axis ko value ho
+
+  path = `M 10 100 Q ${mousemoveBataAakoData.x} ${mousemoveBataAakoData.y} 990 100`;
 
   gsap.to('svg path', {
     attr: { d: path },
@@ -45,10 +90,10 @@ string.addEventListener('mousemove', (receivedValue) => {
   });
 });
 
-string.addEventListener('mouseleave', (receivedValue) => {
+string.addEventListener('mouseleave', (mouseleaveBataAakoData) => {
   gsap.to('svg path', {
     attr: { d: finalPath },
     duration: 4,
-    ease: 'elastic.out(1.5,0.1)',
+    ease: 'elastic.out(1.5, 0.1)',
   });
 });
